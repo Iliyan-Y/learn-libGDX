eb8a3c92e33ec324c0c2b92bf5a57c5916697e0e
Iliyan-Y <ili.yankov91@gmail.com>
2021-03-08 12:35:24 +0000

    First steps to user input -> field added

--- core/src/com/game/drop/Drop.java
+++ core/src/com/game/drop/Drop.java
@@ -3,16 +3,19 @@ package com.game.drop;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.game.drop.screens.MainMenuScreen;
 
 public class Drop extends Game {
     public SpriteBatch batch;
     public BitmapFont font;
+    public Stage stage;
 
   @Override
     public void create() {
       batch = new SpriteBatch();
       font = new BitmapFont();
+      stage = new Stage();
       this.setScreen(new MainMenuScreen(this));
     }
 
@@ -20,6 +23,7 @@ public class Drop extends Game {
   public void dispose() {
     batch.dispose();
     font.dispose();
+    stage.dispose();
   }
 
   @Override
--- core/src/com/game/drop/screens/MainMenuScreen.java
+++ core/src/com/game/drop/screens/MainMenuScreen.java
@@ -2,18 +2,30 @@ package com.game.drop.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.AssetLoader;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.game.drop.Drop;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 public class MainMenuScreen implements Screen {
   final Drop game;
   OrthographicCamera camera;
+  TextField textField;
+  Skin skin;
 
   public MainMenuScreen(final Drop game) {
     this.game = game;
     camera = new OrthographicCamera();
     camera.setToOrtho(false, 800, 480);
+    skin = new Skin(Gdx.files.internal("uiskin.json"));
+    textField = new TextField("", skin);
+    textField.setPosition(200,200);
+    textField.setSize(88, 18);
+    game.stage.addActor(textField);
+    Gdx.input.setInputProcessor(game.stage);
   }
 
   @Override
@@ -30,6 +42,8 @@ public class MainMenuScreen implements Screen {
     game.batch.begin();
     game.font.draw(game.batch, "Welcome to DROP !", 100, 150);
     game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+    game.stage.act(Gdx.graphics.getDeltaTime());
+    game.stage.draw();
     game.batch.end();
 
     if(Gdx.input.isTouched()) {
@@ -60,7 +74,7 @@ public class MainMenuScreen implements Screen {
 
   @Override
   public void dispose() {
-
+      skin.dispose();
   }
 
 }
